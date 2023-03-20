28683467e174d4b1d92df018255d44cc06c04665
VID Neto <dello5@yandex.ru>
2023-03-21 00:14:00 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 12500;
+
+        int milePrice = 20;
+
+        int mile = ticket / milePrice;
+
+        System.out.println("Ваше количество бонусных миль за билет = " + mile);
+    }
+}
